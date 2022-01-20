ed6ddeda4468b2dca7e2632c09179b3e327e7905
manish <mrai8450@gmail.com>
2022-01-20 23:01:25 +0530

    Added client folder

---
+++ client/src/components/404/404.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./404.styles.css"
+
+export default function NotFound() {
+    return (
+        <div className="notfound__text">
+            <h1>
+            We could not find the page you were looking for, the link you followed may be broken, or the page may have been removed.
+            </h1>
+        </div>
+    )
+}
